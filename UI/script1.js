b5264750904fbb4128f300a6acb96f8fab372c63
Kria New Win7 <castlewithwings@gmail.com>
2011-11-18 22:07:58 -0500

    test if it work

---
+++ UI/script1.js
@@ -0,0 +1,18 @@
+
+
+
+(function(){
+	function main(){
+		this.html="hello from kira";
+		
+	}
+	
+	main.prototype().init=function(){
+		alert(this.html);
+	};
+	
+	
+	
+	
+	
+})();
--- UI/test.js
+++ UI/test.js
@@ -1,6 +1,10 @@
 var http = require('http');
+var index=require('./script1')
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end(function(){
+  	var mainpage=new index.main();
+  	mainpage.init();
+  });
 }).listen(1336);
 console.log('Server running at 1336');
